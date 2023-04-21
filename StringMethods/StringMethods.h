3df23809b1e8dce73fa2ffc5db9f132c6ee5c049
Arma Burton <arma@armaburton.com>
2023-04-20 17:54:27 -0700

    reverse a string method works

--- StringMethods/StringMethods.h
+++ StringMethods/StringMethods.h
@@ -1,16 +1,34 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
 class StringMethods {
 public:
   static void reverseString(){
-    string consoleInput;
+    string consoleInput, reverseInput;
 
-    cout << "\nEnter a string:\n";
+
+    cout << "\nEnter a string to reverse:\n";
     cin.ignore();
     getline(cin, consoleInput);
-    cout << "\n" << consoleInput << endl;
+    int inputLength = consoleInput.length();
+    for (int i = 0; i < inputLength / 2;i++){
+      swap(consoleInput[i], consoleInput[inputLength - 1 - i]);
+    }
+
+    cout << consoleInput << endl;
+    
+    char keypress {};
+
+    cout << endl << keypress << endl;
+    do{
+      cout << "\nPress any key to continue...\n"
+           << "? ";
+      keypress = _getch();
+    }while(!keypress);
+    cout << endl;
   }
   
+
 };
