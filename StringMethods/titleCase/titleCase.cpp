9a5d99290428f5c53fa21a3a580aaa2c928fd031
Arma Burton <arma@armaburton.com>
2023-04-21 14:24:57 -0700

    updates

--- StringMethods/titleCase/titleCase.cpp
+++ StringMethods/titleCase/titleCase.cpp
@@ -7,6 +7,10 @@ using namespace std;
 void titleCase(){
   string consoleInput = inputAString("Type a sentence to convert into Title Case.");
 
+  for(int i = 0; i < consoleInput.length(); i++ ){
+    
+  }
+
   cout << endl << consoleInput << endl;
 
   pause();
