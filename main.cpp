cd027a80d4a3c0faf8aeace1480f723da99ec06b
Arma Burton <arma@armaburton.com>
2023-04-20 17:03:05 -0700

    init commit, working on menu

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+  std::string option;
+  while(true){
+    std::cout << "\nC++ String Exercises\n"
+              << "Select an option:\n"
+              << "\n"
+              << "1. Reverse a string\n"
+              << "999. quit\n";
+    std::cin >> option;
+    
+  }
+
+
+
+  return 0;
+}
