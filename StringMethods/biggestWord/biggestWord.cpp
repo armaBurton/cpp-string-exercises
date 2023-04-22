c8ce1b5668af8b0e6303ff760402ee26164cc41e
Arma Burton <arma@armaburton.com>
2023-04-21 17:55:05 -0700

    Find largest word functions correctly

--- StringMethods/biggestWord/biggestWord.cpp
+++ StringMethods/biggestWord/biggestWord.cpp
@@ -5,9 +5,22 @@
 using namespace std;
 
 void biggestWord(){
-  string consoleInput = inputAString("Input a sentence and get the biggest word back.");
+  string consoleInput = inputAString("Input a sentence and get the biggest word back.").append(" ");
   cout << consoleInput << endl;
+  int currentCount = 0, previousCount = 0;
+  string currentBiggest {}, newWord {};
 
+  for (auto letter : consoleInput){
+    if (letter != ' '){
+      newWord = newWord + letter;
+    } else if (letter == ' '){
+      if (newWord.length() > currentBiggest.length()){
+        currentBiggest = newWord;
+      }
+      newWord = "";
+    }
+  }
+  cout << currentBiggest << endl;
 
   pause();
 }
