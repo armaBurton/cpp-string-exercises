f342e50cb3cac84c16d3461befedc5b89ac54486
Arma Burton <arma@armaburton.com>
2023-04-25 16:10:23 -0700

    trying different algorythm

--- StringMethods/consecutiveOnes/consecutiveOnes.cpp
+++ StringMethods/consecutiveOnes/consecutiveOnes.cpp
@@ -5,32 +5,27 @@
 using namespace std;
 
 void consecutiveOnes(){
-  bool prev = false;
+  bool prev = false, current=false;
   int currentCount {}, totalCount {};  
   string consoleInput = inputAString("Type a set of numbers to get the longest sequence of consecutive ones.").append(" ");
 
   for (auto letter : consoleInput){
-    //if letter == 1
-      //if prev = true
-        //current++
-      //prev = true
-      //if current > count
-        //count = current
-    //else if letter != 1
-      //prev = false
-      //current = 0
+    /*
+    if letter = "1"
+      ++currentCount
+      if totalCount < currentCount
+        totalCount = currentCount
+      
+    else if letter != "1"
+      currentCount = 0
+    */
     if (letter == '1'){
-      cout << letter << endl;
-      if (prev == true){
-        currentCount++;
-      }
-      if (currentCount > totalCount){
+      ++currentCount;
+      if (totalCount < currentCount){
         totalCount = currentCount;
+      } else {
+        currentCount = 0;
       }
-      prev = true;
-    }else if (letter != '1'){
-      prev = false;
-      currentCount = 0;
     }
   }
   cout << totalCount << endl;
