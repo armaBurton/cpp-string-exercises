1470247893d87444ff8a557a2cda41908e8168be
Arma Burton <arma@armaburton.com>
2023-04-25 13:58:38 -0700

    testing countedVowels

--- StringMethods/countedVowels/countedVowels.cpp
+++ StringMethods/countedVowels/countedVowels.cpp
@@ -5,5 +5,11 @@
 using namespace std;
 
 void countedVowels(){
+  string consoleInput = inputAString("Type a sentence to get the vowel count.");
+  int count = 0;
+
+  for (auto letter : consoleInput){
+    cout << letter << endl;
+  }
   
 }
