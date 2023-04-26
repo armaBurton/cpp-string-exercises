fa26969bf6b601865530331bd25e0ffd316c8494
Arma Burton <arma@armaburton.com>
2023-04-26 15:05:28 -0700

    cleanup consecutiveOnes

--- StringMethods/consecutiveOnes/consecutiveOnes.cpp
+++ StringMethods/consecutiveOnes/consecutiveOnes.cpp
@@ -10,15 +10,6 @@ void consecutiveOnes(){
   string consoleInput = inputAString("Type a set of numbers to get the longest sequence of consecutive ones.").append(" ");
 
   for (auto letter : consoleInput){
-    /*
-    if letter = "1"
-      ++currentCount
-      if totalCount < currentCount
-        totalCount = currentCount
-      
-    else if letter != "1"
-      currentCount = 0
-    */
     if (letter == '1'){
       ++currentCount;
       if (totalCount < currentCount){
