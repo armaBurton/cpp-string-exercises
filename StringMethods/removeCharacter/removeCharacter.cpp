111eb10e60be6b7d4ef72493852c95ea93e76324
Arma Burton <arma@armaburton.com>
2023-04-26 14:55:55 -0700

    testing

--- StringMethods/removeCharacter/removeCharacter.cpp
+++ StringMethods/removeCharacter/removeCharacter.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <algorithm>
 #include "../inputString/inputString.h"
 #include "../Pause/Pause.h"
 
 using namespace std;
 
 void removeCharacter() {
-  cout << "removeCharacter" << endl;
+  string consoleInput = inputAString("Type a sentence.");
+  string characterInput {};
+  cout << endl 
+       << "Type a single character to remove from the previous sentence." << endl;
+  cin >> characterInput;
+
+
+  consoleInput.erase(remove(consoleInput.begin(), consoleInput.end(), characterInput),consoleInput.end());
+
+  cout << consoleInput << endl;
+
+
+  pause();
 }
