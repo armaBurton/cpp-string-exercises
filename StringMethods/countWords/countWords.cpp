d25b063f07910c0bd5d3bc239695c39b1cff76d2
Arma Burton <arma@armaburton.com>
2023-04-28 18:42:21 -0700

    test

--- StringMethods/countWords/countWords.cpp
+++ StringMethods/countWords/countWords.cpp
@@ -5,5 +5,14 @@
 void countWords(){
   string consoleInput = inputAString("Type a sentence. Then number of words will be returned.");
 
+  for (auto letter : consoleInput){
+    if (letter != ' '){
+
+      cout << letter << endl;
+    } else if (letter == '\n'){
+      cout << "new line\n";
+    }
+  }
+
   pause();
 }
