1355ee539d2e8105a538f5597ad7bf6fb2d0dedd
Arma Burton <arma@armaburton.com>
2023-04-30 12:57:51 -0700

    working so far

--- StringMethods/checkPalindrome/checkPalindrome.cpp
+++ StringMethods/checkPalindrome/checkPalindrome.cpp
@@ -5,5 +5,20 @@
 using namespace std;
 
 void checkPalindrome() {
-  cout << "Check Palindrome" << endl;
+  string consoleInput = inputAString("Enter text for palindrome check.");
+  int length = (consoleInput.length() / 2);
+  bool isPalindrome = true;
+
+  for (int i = 0; i < length; i++){
+    // if (consoleInput[i] != consoleInput[consoleInput.length() - i]){
+    //   isPalindrome = false;
+
+    //   cout << "false\n";
+    //   break;
+    // }
+    cout << consoleInput[i] << " " << consoleInput[consoleInput.length() - 1 - i] << endl;
+  }
+
+
+
 }
