05d837adb7be21bf835376b8de487110da70c59b
Arma Burton <arma@armaburton.com>
2023-04-30 12:58:39 -0700

    testing

--- StringMethods/checkPalindrome/checkPalindrome.cpp
+++ StringMethods/checkPalindrome/checkPalindrome.cpp
@@ -10,14 +10,13 @@ void checkPalindrome() {
   bool isPalindrome = true;
 
   for (int i = 0; i < length; i++){
-    // if (consoleInput[i] != consoleInput[consoleInput.length() - i]){
-    //   isPalindrome = false;
-
-    //   cout << "false\n";
-    //   break;
-    // }
-    cout << consoleInput[i] << " " << consoleInput[consoleInput.length() - 1 - i] << endl;
+    if (consoleInput[i] != consoleInput[consoleInput.length() - 1 - i]){
+      isPalindrome = false;
+      break;
+    }
   }
+
+  cout << isPalindrome << endl;
 
 
 
