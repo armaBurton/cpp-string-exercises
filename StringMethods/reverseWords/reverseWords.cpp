57f88cae2aa3e65eed18a336e07d4483239cd856
Arma Burton <arma@armaburton.com>
2023-05-03 21:51:18 -0700

    pathing error

--- StringMethods/reverseWords/reverseWords.cpp
+++ StringMethods/reverseWords/reverseWords.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "./inputString/inputString.h"
-#include "./Pause/Pause.h"
+#include "../inputString/inputString.h"
+#include "../Pause/Pause.h"
 
 void reverseWords() {
   string consoleInput = inputAString("Enter a sentence to have it's words reversed");
