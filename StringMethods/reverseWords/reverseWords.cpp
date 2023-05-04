108675ceb55a0e43225596e1ccdf21f281dfbd5f
Arma Burton <arma@armaburton.com>
2023-05-03 21:55:54 -0700

    testing loop

--- StringMethods/reverseWords/reverseWords.cpp
+++ StringMethods/reverseWords/reverseWords.cpp
@@ -2,9 +2,21 @@
 #include "../inputString/inputString.h"
 #include "../Pause/Pause.h"
 
+using namespace std;
+
 void reverseWords() {
   string consoleInput = inputAString("Enter a sentence to have it's words reversed");
+  string tempWordHolder {};
   
+  for (auto letter : consoleInput){
+    if (letter != ' '){
+      tempWordHolder += letter;
+    } else {
+      cout << tempWordHolder << endl;
+      tempWordHolder = "";
+    }
+  }
+
   
   pause();
 }
