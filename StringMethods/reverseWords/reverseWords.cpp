7d8eec8b2279600ed0621a595358a3e1dd68151e
Arma Burton <arma@armaburton.com>
2023-05-03 22:03:23 -0700

    testing reverser

--- StringMethods/reverseWords/reverseWords.cpp
+++ StringMethods/reverseWords/reverseWords.cpp
@@ -6,17 +6,24 @@ using namespace std;
 
 void reverseWords() {
   string consoleInput = inputAString("Enter a sentence to have it's words reversed");
-  string tempWordHolder {};
+  string tempWordHolder {},
+         newString {},
+         reversedWord;
+
   
   for (auto letter : consoleInput){
     if (letter != ' '){
       tempWordHolder += letter;
     } else {
-      cout << tempWordHolder << endl;
+      for (int i = tempWordHolder.length() - 1; i >= 0; i--){
+        reversedWord += tempWordHolder[i];
+      }      
+      newString += reversedWord;
       tempWordHolder = "";
+      reversedWord = "";
     }
   }
-
+  cout << newString << endl;
   
   pause();
 }
