e38937c0437fb47b2b1913421d8f067cb105b5cc
Arma Burton <arma@armaburton.com>
2023-05-03 21:49:56 -0700

    testing

---
+++ StringMethods/reverseWords/reverseWords.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "./inputString/inputString.h"
+#include "./Pause/Pause.h"
+
+void reverseWords() {
+  string consoleInput = inputAString("Enter a sentence to have it's words reversed");
+  
+  
+  pause();
+}
